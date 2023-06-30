aad66371bdb63028aae60634409bca93524dd1c2
dhmk083 <dhmk083@gmail.com>
2023-06-30 20:20:16 +0300

    test async to sync

--- app.py
+++ app.py
@@ -15,6 +15,37 @@ DOWNLOADS_DIR = "downloads"
 
 async def echo(websocket):
     async for message in websocket:
+
+        class Logger:
+            def debug(self, msg):
+                # For compatibility with youtube-dl, both debug and info are passed into debug
+                # You can distinguish them by the prefix '[debug] '
+                if msg.startswith("[debug] "):
+                    pass
+                else:
+                    self.info(msg)
+
+            def info(self, msg):
+                self._send(msg)
+
+            def warning(self, msg):
+                self._send(msg)
+
+            def error(self, msg):
+                self._send(msg)
+
+            def _send(self, msg):
+                asyncio.run(websocket.send(msg))
+
+        opts = {"outtmpl": os.path.join(DOWNLOADS_DIR, "1.webm"), "logger": Logger()}
+
+        def download():
+            with YoutubeDL(opts) as ydl:
+                ydl.download(["https://www.youtube.com/watch?v=zGDzdps75ns"])
+
+        loop = asyncio.get_running_loop()
+        await loop.run_in_executor(None, download)
+
         await websocket.send(message)
 
 
