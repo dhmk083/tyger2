187e466df232a693de0ee97a7196a49a8a1e539c
dhmk083 <dhmk083@gmail.com>
2023-08-02 14:31:47 +0300

    add partial download

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import asyncio
 import http
 import signal
 import os
+import re
 from pathlib import PurePath
 from urllib.parse import unquote
 from mimetypes import guess_type
@@ -82,7 +83,7 @@ async def echo(websocket):
         await websocket.close()
 
 
-def serve_file(path):
+def serve_file(path, headers):
     try:
         with open(path, "rb") as f:
             mime = guess_type(path)[0]
@@ -93,7 +94,53 @@ def serve_file(path):
             if path.endswith(".m4a"):
                 mime = "audio/mp4"
 
-            return http.HTTPStatus.OK, {"Content-Type": mime}, f.read()
+            f.seek(0, os.SEEK_END)
+            file_size = f.tell()
+
+            start = 0
+            end = file_size
+
+            range_header = headers.get_all("Range")
+            if range_header:
+                try:
+                    m = re.fullmatch(r"bytes=(\d*)-(\d*)", range_header[0])
+                    if not m:
+                        raise AssertionError()
+
+                    start, end = [int(x) if x else None for x in m.groups()]
+
+                    bad = start is None and end is None
+                    bad_start = (start or False) >= (end or file_size)
+                    bad_end = (end or False) > file_size
+                    if bad or bad_start or bad_end:
+                        raise AssertionError()
+
+                    if start is None:
+                        start = file_size - end
+                        end = file_size
+
+                    if end is None:
+                        end = file_size
+                except AssertionError:
+                    return http.HTTPStatus.REQUESTED_RANGE_NOT_SATISFIABLE, [], b""
+
+            f.seek(start)
+            range_size = end - start
+
+            headers = {
+                "Content-Type": mime,
+                "Content-Length": range_size,
+                "Accept-Ranges": "bytes",
+            }
+
+            if range_header:
+                headers["Content-Range"] = f"bytes {start}-{end}/{file_size}"
+
+            status = (
+                http.HTTPStatus.PARTIAL_CONTENT if range_header else http.HTTPStatus.OK
+            )
+
+            return status, headers, f.read(range_size)
     except FileNotFoundError:
         return http.HTTPStatus.NOT_FOUND, [], b""
 
@@ -103,11 +150,11 @@ async def process_request(path, request_headers):
         return http.HTTPStatus.OK, [], b"OK\n"
 
     if path == "/":
-        return serve_file("index.html")
+        return serve_file("index.html", request_headers)
 
     if path.startswith("/" + DOWNLOADS_DIR):
         filepath = unquote(path[1:])
-        return serve_file(filepath)
+        return serve_file(filepath, request_headers)
 
 
 async def main():
